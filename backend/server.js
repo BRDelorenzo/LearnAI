ff97e05e02e059cf7d1f67a4e267b538fa1f166e
BRDelorenzo <giovanni.delorenzo1998@gmail.com>
2025-12-03 23:06:55 -0300

    atualizando o app

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ import { OpenAI } from 'openai';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
 // __dirname em ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
